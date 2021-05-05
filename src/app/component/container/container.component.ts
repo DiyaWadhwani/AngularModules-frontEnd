66815a72e20434178f33eaa14d02e541521cc3fe
Diya DW <diya@wolkensoftware.com>
2021-05-05 21:56:29 +0530

    updates - Field Validations

--- src/app/component/container/container.component.ts
+++ src/app/component/container/container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-container',
@@ -7,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : ActivatedRoute, _router : Router) { }
 
   function : string;
   title = "Wolken Job Portal";
+  navDisplay : string = "Register";
+  pathName : string = " ";
+  // _router : string;
 
   ngOnInit(): void {
+    let body = JSON.stringify(this.route.url);
+    // console.log("URL : " + this.route.snapshot.params); 
+    console.log("URL : "+window.location.pathname);
+
+    this.pathName = window.location.pathname;
+
+    if(this.pathName == '/employee-login') {
+      this.navDisplay == "Register";
+    }
+    else if(this.pathName == '/employee-registration') {
+      this.navDisplay == "Login";
+    }
+
   }
 
 }
